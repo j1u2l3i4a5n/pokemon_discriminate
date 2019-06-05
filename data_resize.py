96502092d709440a0f0f1fc2f1614be458407d3e
j1u2l3i4a5n <b04207001@ntu.edu.tw>
2019-06-05 11:14:24 +0800

    resize data and save to .npy form

---
+++ data_resize.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  3 21:14:37 2019
+
+@author: rjlin
+"""
+
+import os 
+import cv2
+import numpy as np
+from matplotlib.image import imread
+from matplotlib import pyplot as plt
+
+image_path1 = os.listdir('downloads/')
+
+for i in image_path1:
+    data = []
+    image_paths2 = os.listdir('downloads/%s'%i)
+    for j in image_paths2:
+        try:
+            image = cv2.imread('downloads/%s/%s'%(i,j))
+            image = cv2.resize(image, (100,100))
+            if image.dtype != 'float':
+                image = image / 255
+            data.append(image)
+        except Exception as e:
+            try:
+                image = imread('downloads/%s/%s'%(i,j))
+                if image.shape[2] == 4:
+                    image = np.delete(image, 3, 2)
+                
+                image = cv2.resize(image, (100,100))
+                if image.dtype != 'float':
+                    image = image / 255
+                    data.append(image)
+            except:
+                pass
+            
+    data = np.array(data)
+    np.save('data/%s.npy'%i, data)
+''' 
+data_path = os.listdir('data')
+ 
+for i in data_path:
+    try:
+        matrix = np.load('data/%s'%i)
+        if matrix.shape[0] < 20:
+            print(i)
+            print(matrix.shape)
+    except:
+        pass
+'''
